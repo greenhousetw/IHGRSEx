4b7c82e1bccd2b0da96d02579a76d3c98a486b3f
greenhousetw <greenhousetw@outlook.com>
2014-07-01 19:06:00 +0800

    change algorithm

--- src/test/UnitTestCollection/TestSimpleSectionAlgorithm/tomato.js
+++ src/test/UnitTestCollection/TestSimpleSectionAlgorithm/tomato.js
@@ -1,8 +1,18 @@
-function Temprature(value){
+function Temprature(lastTime, value){
     var jsonString="";
 
-    if(parseFloat(value) > 99){
-        jsonString="{\"section\": \"1\",\"equipments\": [ {\"fan\": \"fan.on\"},{\"light\": \"light.on\"}]}";
+    var timeDiff=0;
+
+    if(parseFloat(value) > 99 ){
+
+        if(timeDiff ==0 || timeDiff < 0 )
+        {
+            jsonString="{\"section\": \"1\",\"equipments\": [ {\"fan\": \"fan.on\"},{\"light\": \"light.on\"}]}";
+        }
+        else
+        {
+            jsonString="{\"section\": \"1\",\"lasttime\": \"Time\"}";
+        }
     }
     else if (parseFloat(value)<50 && parseFloat(value) > 0)
     {
@@ -16,13 +26,13 @@ function Temprature(value){
     return jsonString;
 };
 
-function Humid(value){
+function Humid(lastTime, value){
     if(parseFloat(value) > 99){
        jsonString="[{'section':'1'},{'equipment':'fan','status':'on'},{'equipment':'light','status':'off'}]";
     }
 };
 
-function Light(value){
+function Light(lastTime, value){
     if(parseFloat(value) > 99){
        jsonString="[{'section':'1'},{'equipment':'fan','status':'on'},{'equipment':'light','status':'off'}]";
     }
